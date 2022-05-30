f611713d06c695d2cc25077b9c80c69718f66bb8
LQ85i <39389364+LQ85i@users.noreply.github.com>
2022-05-30 12:34:26 +0300

    Add some input writing on the calculator screen. Calculator still non-functional.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const inputList = [];
+
 function operate(n1,n2,operator) {
     if (operator = "add"){
         add(n1,n2);
@@ -27,7 +29,122 @@ function divide(n1,n2){
     return n1 / n2;
 }
 
-const additionButton = document.querySelector('.operator #add');
-const substractButton = document.querySelector('.operator #substract');
-const multiplyButton = document.querySelector('.operator #multiply');
-const divideButton = document.querySelector('.operator #divide');
+function storeInput(input){
+    if(inputList.length<40){
+        if(!isNaN(input)){
+            inputList.push(input);
+        }
+        else if(input == "add"){
+            if(!isNaN(inputList[inputList.length-1])){
+                inputList.push('+');
+            }
+        }
+        else if(input == "substract"){
+            if(inputList[inputList.length-1] != "-"){
+                inputList.push('-');
+            }
+        }
+        else if(input == "multiply"){
+            if(!isNaN(inputList[inputList.length-1])){
+                inputList.push('*');
+            }
+        }
+        else if(input == "divide"){
+            if(!isNaN(inputList[inputList.length-1])){
+                inputList.push('/');
+            }
+        }
+        else if(input == "leftParentheses"){
+            inputList.push('(');
+        }
+        else if(input == "rightParentheses"){
+            inputList.push(')');
+        }
+        
+        else if(input == "point"){
+            if(!inputList.includes('.') && !isNaN(inputList[inputList.length-1])){
+                inputList.push('.');
+            }
+        }
+    }
+    if(input == "clear"){
+        inputList.length = 0;
+    }
+    else if(input == "backspace"){
+        inputList.pop();
+    }
+    
+    processInput();
+    updateOutput();
+}
+
+function processInput(){
+    return;
+}
+
+function updateOutput(){
+    if(inputList.length>=33){
+        document.getElementById("output").style.fontSize = 10 + "px";
+    }
+    else if(inputList.length>=28){
+        document.getElementById("output").style.fontSize = 12 + "px";
+    }
+    else if(inputList.length>=23){
+        document.getElementById("output").style.fontSize = 15 + "px";
+    }
+    else if(inputList.length>=18){
+        document.getElementById("output").style.fontSize = 17 + "px";
+    }
+    else if(inputList.length<18){
+        document.getElementById("output").style.fontSize = 22 + "px";
+    }
+    document.getElementById("output").innerHTML = inputList.join("");
+    
+}
+
+
+const buttonAdd = document.querySelector('#add');
+const buttonSubstract = document.querySelector('#substract');
+const buttonMultiply = document.querySelector('#multiply');
+const buttonDivide = document.querySelector('#divide');
+
+const button0 = document.querySelector('#zero');
+const button1 = document.querySelector('#one');
+const button2 = document.querySelector('#two');
+const button3 = document.querySelector('#three');
+const button4 = document.querySelector('#four');
+const button5 = document.querySelector('#five');
+const button6 = document.querySelector('#six');
+const button7 = document.querySelector('#seven');
+const button8 = document.querySelector('#eight');
+const button9 = document.querySelector('#nine');
+
+const buttonPoint = document.querySelector('#point');
+const buttonBackspace = document.querySelector('#backspace');
+const buttonEquals = document.querySelector('#equals');
+const buttonClear = document.querySelector('#clear');
+const buttonLeftParentheses = document.querySelector('#leftParentheses');
+const buttonRightParentheses = document.querySelector('#rightParentheses');
+
+buttonAdd.addEventListener("click", function(){storeInput("add"); });
+buttonSubstract.addEventListener("click", function(){storeInput("substract"); });
+buttonMultiply.addEventListener("click", function(){storeInput("multiply"); });
+buttonDivide.addEventListener("click", function(){storeInput("divide"); });
+
+button0.addEventListener("click", function(){storeInput(0); });
+button1.addEventListener("click", function(){storeInput(1); });
+button2.addEventListener("click", function(){storeInput(2); });
+button3.addEventListener("click", function(){storeInput(3); });
+button4.addEventListener("click", function(){storeInput(4); });
+button5.addEventListener("click", function(){storeInput(5); });
+button6.addEventListener("click", function(){storeInput(6); });
+button7.addEventListener("click", function(){storeInput(7); });
+button8.addEventListener("click", function(){storeInput(8); });
+button9.addEventListener("click", function(){storeInput(9); });
+
+buttonPoint.addEventListener("click", function(){storeInput("point"); });
+buttonBackspace.addEventListener("click", function(){storeInput("backspace"); });
+buttonEquals.addEventListener("click", function(){storeInput("equals"); });
+buttonClear.addEventListener("click", function(){storeInput("clear"); });
+buttonLeftParentheses.addEventListener("click", function(){storeInput("leftParentheses"); });
+buttonRightParentheses.addEventListener("click", function(){storeInput("rightParentheses"); });
