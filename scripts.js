9cdf5e9ee3e1e1fdee44b1fc75825cd3179db7bb
Funxy <jaakko.kakko97@gmail.com>
2022-06-17 04:09:17 +0300

    Add most of calculator functionality. Negation still missing.

--- scripts.js
+++ scripts.js
@@ -1,68 +1,116 @@
-const inputList = [];
-
-function operate(n1,n2,operator) {
-    if (operator = "add"){
-        add(n1,n2);
-    }
-    else if (operator = "subtract"){
-        substract(n1,n2);
-    }
-    else if (operator = "multiply"){
-        multiply(n1,n2);
-    }
-    else if (operator = "divide"){
-        divide(n1,n2);
-    }
-}
+let inputList = [];
+const operators = ["+","-","*","/"];
 
 function add(n1,n2){
-    return n1 + n2;
+    const r = n1+n2;
+    return Math.round(r * 1000000000) / 1000000000;
 }
 function substract(n1,n2){
-    return n1 - n2;
+    const r = n1-n2;
+    return Math.round(r * 1000000000) / 1000000000;
 }
 function multiply(n1,n2){
-    return n1 * n2;
+    const r = n1*n2;
+    return Math.round(r * 1000000000) / 1000000000;
 }
 function divide(n1,n2){
-    if(n2 == 0){return "ERROR: Division by zero"};
-    return n1 / n2;
+    if(n2 == 0){return NaN};
+    const r = n1/n2;
+    return Math.round(r * 1000000000) / 1000000000;
+}
+
+function operate(n1,n2,operator) {
+    if (operator == "+"){
+        return add(n1,n2);
+    }
+    else if (operator == "-"){
+        return substract(n1,n2);
+    }
+    else if (operator == "*"){
+        return multiply(n1,n2);
+    }
+    else if (operator == "/"){
+        return divide(n1,n2);
+    }
 }
 
 function storeInput(input){
+    let last = inputList[inputList.length-1];
+    let secondToLast = null;
+    if(input.length>1){
+        secondToLast = inputList[inputList.length-2];
+    }
     if(inputList.length<40){
         if(!isNaN(input)){
             inputList.push(input);
         }
         else if(input == "add"){
-            if(!isNaN(inputList[inputList.length-1])){
+            if(operators.includes(last) && secondToLast != "(" && !operators.includes(secondToLast)){
+                inputList.pop();
+                inputList.push("+");
+            }
+            else if(!isNaN(last) || last == ")"){
                 inputList.push('+');
             }
         }
         else if(input == "substract"){
-            if(inputList[inputList.length-1] != "-"){
+            if(last != "-" && last != "."){
                 inputList.push('-');
             }
         }
         else if(input == "multiply"){
-            if(!isNaN(inputList[inputList.length-1])){
+            if(operators.includes(last) && secondToLast != "(" && !operators.includes(secondToLast)){
+                inputList.pop();
+                inputList.push("*");
+            }
+            else if(!isNaN(last) || last == ")"){
                 inputList.push('*');
             }
         }
         else if(input == "divide"){
-            if(!isNaN(inputList[inputList.length-1])){
+            if(operators.includes(last) && secondToLast != "(" && !operators.includes(secondToLast)){
+                inputList.pop();
+                inputList.push("/");
+            }
+            else if(!isNaN(last) || last == ")"){
                 inputList.push('/');
             }
         }
         else if(input == "leftParentheses"){
-            inputList.push('(');
+            if((last != "." && isNaN(last)) || operators.includes(last)){
+                inputList.push('(');
+            }
         }
         else if(input == "rightParentheses"){
-            inputList.push(')');
+            let left = 0;
+            let right = 0;
+            for(let i = 0; i < inputList.length; i++){
+                if(inputList[i] == "("){
+                    left++;
+                }
+                else if(inputList[i] == ")"){
+                    right++;
+                }
+            }
+            if(left > right){
+                inputList.push(')');
+            }
         }
         
         else if(input == "point"){
-            if(!inputList.includes('.') && !isNaN(inputList[inputList.length-1])){
+            let allowPoint = true;
+            for(let i = inputList.length - 1; i >= 0; i--){
+                const item = inputList[i];
+                if(item == "."){
+                    allowPoint = false;
+                    break;
+                }
+                else if(operators.includes(item)){
+                    allowPoint = true
+                    break;
+                }
+            }
+            if(!isNaN(last) && allowPoint){
                 inputList.push('.');
             }
         }
@@ -73,13 +121,245 @@ function storeInput(input){
     else if(input == "backspace"){
         inputList.pop();
     }
-    
-    processInput();
+    else if(input == "equals" && inputList.length>0){
+        while(operators.includes(last) || last == "." || last == "("){
+            inputList.pop();
+            last = inputList[inputList.length-1];
+        }
+        let left = 0;
+        let right = 0;
+        for(let i = 0; i < inputList.length; i++){
+            if(inputList[i] == "("){
+                left++;
+            }
+            else if(inputList[i] == ")"){
+                right++;
+            }
+        }
+        while(right < left){
+            inputList.push(')');
+            right++;
+        }
+        inputList = processInput(inputList);
+    }
     updateOutput();
 }
 
-function processInput(){
-    return;
+function processInput(input){
+    console.log(input.join(""));
+    let result = input;
+    // compute parentheses here
+    if(input.includes("(")){
+        for(let i = 0; i < input.length;i++){
+            if(input[i] == "("){
+                let depth = 1;
+                let leftIndex = i+1;
+                let j = i+1;
+                while(depth > 0 && j < input.length){
+                    if(input[j] == "("){
+                        depth++;
+                    }
+                    else if(input[j] == ")"){
+                        depth--;
+                    }
+                    j++;
+                }
+                let rightIndex = j-1;
+                const subInput = input.slice(leftIndex,rightIndex);
+    
+    
+                
+                const arrayEnd = result.slice(rightIndex+1);
+                result = result.slice(0, leftIndex-1);
+                result = result.concat(processInput(subInput));
+                result = result.concat(arrayEnd);
+                console.log("now it looks like: " + result);
+                i = j+1;
+            }
+        }
+    }
+    // compute * and / here
+    if(result.includes("*") || result.includes("/")){
+        let n1LeftIndex = null;
+        let n1RightIndex = null;
+        let n2LeftIndex = null;
+        let n2RightIndex = null;
+        let n1 = null;
+        let n2 = null;
+        let operator = null;
+        for(let i = 0; i < result.length;i++){
+            if(result[i] == "*" || result[i] == "/"){
+                n1RightIndex = i-1;
+                n2LeftIndex = i+1;
+                operator = result[i];
+                //  -12       *       34
+                //       |operator|        
+                // | n1|
+                //                   |n2|
+                // 
+                // operate(n1,n2,operator)
+                for(let j = i-1; j >= 0;j--){
+                    if(j==0){
+                        n1LeftIndex = j;
+                        break;
+                    }
+                    else if(operators.includes(result[j])){
+                        if(result[j] == "-"){
+                            // if symbol before - is operator, include - to number
+                            if(isNan(result[j-1])){
+                                n1LeftIndex = j;
+                                break;
+                            }
+                            else {
+                                n1LeftIndex = j+1;
+                                break;
+                            }
+                        }
+                        else{
+                            n1LeftIndex = j+1;
+                            break;
+                        }
+                    }
+                    else if(result[j] == "."){
+                        continue;
+                    }
+                }
+                for(let j = i+1; j < result.length;j++){
+                    if(j == result.length - 1){
+                        n2RightIndex = j;
+                        break;
+                    }
+                    else if(operators.includes(result[j])){
+                        if(result[j]=="-" && j == i+1){
+                            continue;
+                        }
+                        n2RightIndex = j-1;
+                        break;
+                    }
+                }
+                console.log("n1 indexes: ",n1LeftIndex,n1RightIndex);
+                n1 = Number(result.slice(n1LeftIndex,n1RightIndex+1).join(""));
+                console.log("n1: "+n1);
+
+                console.log("n2 indexes: ",n2LeftIndex,n2RightIndex);
+                n2 = Number(result.slice(n2LeftIndex,n2RightIndex+1).join(""));
+                console.log("n2: "+n2);
+
+                subResult = operate(n1,n2,operator);
+                console.log("subResult: "+subResult);
+
+                const arrayEnd = result.slice(n2RightIndex+1);
+                console.log("part after result: "+arrayEnd);
+                
+                if(n1LeftIndex>1){
+                    result = result.slice(0, n1LeftIndex);
+                }
+                else {
+                    result = [];
+                }
+                console.log("part before result: "+result);
+
+                result = result.concat(subResult);
+                console.log("result with start: "+result);
+
+                result = result.concat(arrayEnd);
+                console.log("result with end: "+result);
+
+                i = n1LeftIndex;
+                console.log("new i index: ",i);
+            }
+        }
+    }
+    // compute + and - here
+    if(result.includes("+") || result.includes("-")){
+        let n1LeftIndex = null;
+        let n1RightIndex = null;
+        let n2LeftIndex = null;
+        let n2RightIndex = null;
+        let n1 = null;
+        let n2 = null;
+        let operator = null;
+        for(let i = 0; i < result.length;i++){
+            if(result[i] == "+" || result[i] == "-"){
+                n1RightIndex = i-1;
+                n2LeftIndex = i+1;
+                operator = result[i];
+                //  -12       +       34
+                //       |operator|        
+                // | n1|
+                //                   |n2|
+                // 
+                // operate(n1,n2,operator)
+                for(let j = i-1; j >= 0;j--){
+                    if(j==0){
+                        n1LeftIndex = j;
+                        break;
+                    }
+                    else if(operators.includes(result[j])){
+                        if(result[j] == "-"){
+                            // if symbol before - is operator, include - to number
+                            if(isNan(result[j-1])){
+                                n1LeftIndex = j;
+                                break;
+                            }
+                            else {
+                                n1LeftIndex = j+1;
+                                break;
+                            }
+                        }
+                        else{
+                            n1LeftIndex = j+1;
+                            break;
+                        }
+                    }
+                }
+                for(let j = i+1; j < result.length;j++){
+                    if(j == result.length - 1){
+                        n2RightIndex = j;
+                        break;
+                    }
+                    else if(operators.includes(result[j])){
+                        n2RightIndex = j-1;
+                        break;
+                    }
+                }
+
+                console.log("n1 indexes: ",n1LeftIndex,n1RightIndex);
+                n1 = Number(result.slice(n1LeftIndex,n1RightIndex+1).join(""));
+                console.log("n1: "+n1);
+
+                console.log("n2 indexes: ",n2LeftIndex,n2RightIndex);
+                n2 = Number(result.slice(n2LeftIndex,n2RightIndex+1).join(""));
+                console.log("n2: "+n2);
+
+                subResult = operate(n1,n2,operator);
+                console.log("subResult: "+subResult);
+
+                const arrayEnd = result.slice(n2RightIndex+1);
+                console.log("part after result: "+arrayEnd);
+                
+                if(n1LeftIndex>1){
+                    result = result.slice(0, n1LeftIndex-1);
+                }
+                else {
+                    result = [];
+                }
+                console.log("part before result: "+result);
+
+                result = result.concat(subResult);
+                console.log("result with start: "+result);
+
+                result = result.concat(arrayEnd);
+                console.log("result with end: "+result);
+
+                i = n1LeftIndex;
+                console.log("new i index: ",i);
+            }
+        }
+    }
+    console.log("return result: ",result.join(""));
+    console.log("reduce depth");
+    return result;
 }
 
 function updateOutput(){
